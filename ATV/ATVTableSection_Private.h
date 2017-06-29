66db64d60ba7aae467ca4fe72ff9c03d03ac8d99
Jason Sypher <jason.sypher@eleostech.com>
2017-06-29 14:39:04 -0600

    Removing macros that defined the tableViews memory retention and replaced it with 'weak'
    Seems to work but haven’t tested it very much

--- ATV/ATVTableSection_Private.h
+++ ATV/ATVTableSection_Private.h
@@ -1,19 +1,8 @@
 #import "ATVTableSection.h"
 
 @class ATVTableView;
-
-#ifndef ATV_WEAK
-#if __has_feature(objc_arc_weak)
-#define ATV_WEAK weak
-#elif __has_feature(objc_arc)
-#define ATV_WEAK unsafe_unretained
-#else
-#define ATV_WEAK assign
-#endif
-#endif
-
 @interface ATVTableSection ()
-@property (ATV_WEAK) ATVTableView* _tableView;
+@property (weak) ATVTableView* _tableView;
 @property (strong) NSMutableDictionary* registeredNibs;
 @property BOOL needsToPerformUpdates;
 @end
